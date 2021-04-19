8cc5ca0c9bb791c0cde39221f8ffc45c68cf407c
stachowb <stachow.bartlomiej@gmail.com>
2021-04-20 01:43:09 +0200

    initial

--- shifts/models.py
+++ shifts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Shift(models.Model):
+    driver_name = models.CharField(max_length=60)
+    company_name = models.CharField(max_length=60)
+
+    EQ_TYPES = (
+        (1, "Trekker"),
+        (2, "Containerbil m/løftelem"),
+        (3, "Containerbil"),
+        (4, "-")
+    )
+    eq_type = models.CharField(max_length=60, choices=EQ_TYPES)
+    clock_in_date = models.CharField(max_length=60)
+    clock_out_date = models.CharField(max_length=60)
+    km_driven = models.IntegerField(default=0)
--- shifts/views.py
+++ shifts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views import View
+from shifts.models import Shift
+from django.http import HttpResponse
 
-# Create your views here.
+
+class HomePage(View):
+    def get(self, request):
+        shifts = Shift.objects.all()
+        return render(request, "homepage.html", {"shifts": shifts})
+
+    def post(self, request):
+        driver_name = request.POST.get('driver_name')
+        company_name = request.POST.get('company_name')
+        eq_type = request.POST.get('eq_type')
+        clock_in = request.POST.get('clock_in')
+        clock_out = request.POST.get('clock_out')
+        km_driven = int(request.POST.get('km_driven'))
+        Shift.objects.create(driver_name=driver_name, company_name=company_name, eq_type=eq_type, clock_in_date=clock_in,
+                             clock_out_date=clock_out, km_driven=km_driven)
+
+        return HttpResponse("record added")
+
