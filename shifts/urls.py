a72b3ec0806136dacc10cdc0034c3c8477784d29
stachowb <stachow.bartlomiej@gmail.com>
2021-04-27 21:31:24 +0200

    initial

--- shifts/urls.py
+++ shifts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ShiftList, ShiftAdd
 
 urlpatterns = [
-    path('shifts/shift-list/', ShiftList.as_view(), name="shift-list"),
-    path('shifts/add-shift/', ShiftAdd.as_view(), name="shift-add")
+    path('shift-list/', ShiftList.as_view(), name="shift-list"),
+    path('add-shift/', ShiftAdd.as_view(), name="shift-add")
 ]
