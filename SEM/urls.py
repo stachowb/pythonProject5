1ee1b370ed65cb803e969752f27f015def149ba6
stachowb <stachow.bartlomiej@gmail.com>
2021-04-30 23:57:08 +0200

    initial

--- SEM/urls.py
+++ SEM/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from shifts.views import HomePage
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', HomePage.as_view(), name="home"),
     path('shifts/', include("shifts.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_URL)
+
